7a0a91c1853ad78f580e0a0c39f29addb4ab9f76
Abdullah9001 <abdullahjubayer44@gmail.com>
2021-12-17 14:53:02 +0600

    Completed

---
+++ app.js
@@ -0,0 +1,39 @@
+gsap.to("#img", {
+  scrollTrigger : {
+    scrub: 0.5,
+  },
+  x : 1500,
+  y : 500,
+})
+
+
+gsap.from("#wolf", {
+  scrollTrigger : {
+    scrub: 0.5,
+  },
+  x : 400,
+})
+
+gsap.from("#castle", {
+  scrollTrigger : {
+    scrub: 0.5,
+  },
+  x : -100,
+})
+
+
+gsap.to("#bats", {
+  scrollTrigger : {
+    scrub: 0.5,
+  },
+  scale : 1.5,
+})
+
+
+gsap.to("#text", {
+  scrollTrigger : {
+    scrub: 0.5,
+  },
+  y : 500,
+})
+
